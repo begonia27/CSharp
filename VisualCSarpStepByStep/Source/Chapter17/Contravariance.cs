de30899c9d7d8e85a564b87267cec843c187008e
io <1134921599@qq.com>
2024-04-09 20:29:53 +0800

    Collection list and linkedList

---
+++ VisualCSarpStepByStep/Source/Chapter17/Contravariance.cs
@@ -0,0 +1,38 @@
+﻿namespace Source.Chapter17;
+
+public interface IComparer<in T>
+{
+    int Compare(T x, T y);
+}
+public class ObjectComparer : IComparable<Object>, IComparable<ObjectComparer>, IComparable, IComparer<object>
+{
+    int IComparer<Object>.Compare(Object x, Object y)
+    {
+        int xHash = x.GetHashCode();
+        int yHash = y.GetHashCode();
+        if (xHash == yHash)
+        {
+            return 0;
+        }
+        else if (xHash < yHash)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public int CompareTo(ObjectComparer? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CompareTo(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return 1;
+        if (ReferenceEquals(this, obj)) return 0;
+        return obj is ObjectComparer other ? CompareTo(other) : throw new ArgumentException($"Object must be of type {nameof(ObjectComparer)}");
+    }
+}
---
+++ VisualCSarpStepByStep/UnitTests/Chapter17/ContravarianceTest.cs
@@ -0,0 +1,19 @@
+﻿namespace UnitTests.Chapter17;
+using Source.Chapter17;
+
+[TestClass]
+public class ContravarianceTests
+{
+    [TestMethod]
+    public void Contravariance()
+    {
+        Object x = 1;
+        Object y = 4;
+
+        ObjectComparer objectComparer = new ObjectComparer();
+        IComparer<Object> objectComparator = objectComparer;
+        int result = objectComparator.Compare(x, y);
+
+        Assert.AreEqual(-1,result);
+    }
+}
