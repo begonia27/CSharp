0dd858f2da423113c07fcface9598e26096cb65e
io <1134921599@qq.com>
2024-04-16 09:37:06 +0800

    Use delegates with an instance

---
+++ VisualCSarpStepByStep/Source/Chapter20/FindInstance.cs
@@ -0,0 +1,15 @@
+﻿namespace Source.Chapter20;
+
+public struct Person
+{
+    public int ID { set; get; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person()
+    {
+        ID = 0;
+        Name = null;
+        Age = 0;
+    }
+}
---
+++ VisualCSarpStepByStep/UnitTests/Chapter20/FindInstanceTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using Source.Chapter20;
+
+namespace UnitTests.Chapter20;
+
+[TestClass]
+public class FindInstanceTests
+{
+    [TestMethod]
+    public void FindFamilyInfo()
+    {
+        List<Person> personnel =
+        [
+            new Person() { ID = 1, Name = "John", Age = 53 },
+            new Person() { ID = 2, Name = "Sid", Age = 28 },
+            new Person() { ID = 3, Name = "Fred", Age = 34 },
+            new Person() { ID = 4, Name = "Paul", Age = 22 }
+        ];
+
+        double averageAge = personnel.Average(p => p.Age);
+        double expectedAge = 34.25;
+
+        int maxID = personnel.Max(p => p.ID);
+        int expectedId = 4;
+
+        int thirties = personnel.Count(p => p.Age >= 30 && p.Age <= 39);
+        int expectedThitries = 1;
+        
+        Assert.AreEqual(expectedAge, averageAge);
+        Assert.AreEqual(expectedId, maxID);
+        Assert.AreEqual(expectedThitries, thirties);
+    }
+}
