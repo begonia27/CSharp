32a3ab7b7796fc4817f4ed78a387e702c94c034e
io <1134921599@qq.com>
2024-04-19 18:25:34 +0800

    operator overload

---
+++ VisualCSarpStepByStep/Source/Chapter22/ComplexNumbers.cs
@@ -0,0 +1,71 @@
+﻿namespace Source.Chapter22;
+
+public struct Complex
+{
+    public int Real { get; set; }
+    public int Imaginary { get; set; }
+
+    public Complex(int real, int imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+
+    public Complex(int real)
+    {
+        Real = real;
+        Imaginary = 0;
+    }
+    
+    // implicit conversion: int => Complex
+    public static implicit operator Complex(int from) => new Complex(from);
+    
+    // explicit conversion: Complex => int
+    public static explicit operator int(Complex from) => from.Real;
+
+    public override string ToString() => $"{Real} + {Imaginary}i";
+
+    public static Complex operator +(Complex lhs, Complex rhs) =>
+        new Complex(lhs.Real + rhs.Real, lhs.Imaginary + rhs.Imaginary);
+
+    public static Complex operator -(Complex lhs, Complex rhs) =>
+        new Complex(lhs.Real - rhs.Real, lhs.Imaginary - rhs.Imaginary);
+
+    public static Complex operator *(Complex lhs, Complex rhs) =>
+        new Complex(lhs.Real * rhs.Real - lhs.Imaginary * rhs.Imaginary,
+            lhs.Imaginary * rhs.Real + lhs.Real * rhs.Imaginary);
+
+    public static Complex operator /(Complex lhs, Complex rhs)
+    { 
+        int realElement = (lhs.Real * rhs.Real + lhs.Imaginary * rhs.Imaginary) /
+                        (rhs.Real * rhs.Real + rhs.Imaginary * rhs.Imaginary);
+        
+        int imaginaryElement = (lhs.Imaginary * rhs.Real - lhs.Real * rhs.Imaginary) /
+                               (rhs.Real * rhs.Real + rhs.Imaginary * rhs.Imaginary);
+
+        return new Complex(realElement, imaginaryElement);
+    }
+    
+    
+    public static bool operator ==(Complex lhs, Complex rhs) => lhs.Equals(rhs);
+
+    public static bool operator !=(Complex lhs, Complex rhs) => !(lhs == rhs);
+    public override bool Equals(Object obj)
+    {
+        if (obj is Complex)
+        {
+            Complex compare = (Complex)obj;
+            return (Real == compare.Real)
+                   && (Imaginary == compare.Imaginary);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
---
+++ VisualCSarpStepByStep/UnitTests/Chapter22/TestOnly.cs
@@ -0,0 +1,46 @@
+﻿namespace UnitTests.Chapter22;
+
+[TestClass]
+public class TestOnly
+{
+    class Hour
+    {
+        public int Value;
+        
+        public static Hour operator ++(Hour hour)
+        {
+            hour.Value++;
+            
+            return hour;
+
+            // return new Hour() { Value = hour.Value + 1 };
+        }
+    }
+    [TestMethod]
+    public void T()
+    {
+        // Hour h = new Hour();
+        //
+        // h.Value++;
+        //
+        // Console.WriteLine(h.Value);
+        //
+        // h++;
+        //
+        // Console.WriteLine(h.Value);
+
+        Hour prefix = new Hour();
+        Hour postfix = prefix;
+
+        postfix = prefix;
+        prefix++;
+        
+        Console.WriteLine(prefix.Value);
+        Console.WriteLine(postfix.Value);
+
+        prefix = ++postfix;
+        
+        Console.WriteLine(prefix.Value);
+        Console.WriteLine(postfix.Value);
+    }
+}
